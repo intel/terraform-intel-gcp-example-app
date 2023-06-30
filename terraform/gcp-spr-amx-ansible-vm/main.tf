d495c6615b9e26f8dfb763f1cc5cb47a732f91d4
lucasmelogithub <lucas.melo@intel.com>
2023-06-30 16:16:31 -0500

    Change disk type and size

--- terraform/gcp-spr-amx-ansible-vm/main.tf
+++ terraform/gcp-spr-amx-ansible-vm/main.tf
@@ -8,8 +8,7 @@ module "linux_vm" {
   project             = var.project
   boot_image_project  = "ubuntu-os-cloud"
   boot_image_family   = "ubuntu-2204-lts"
-  boot_disk_type      = "pd-extreme"
-  boot_disk_size      = 200
+  boot_disk_size      = 500
   name                = "lmelo-spr-amx-ansible-vm"
   zone                = "us-central1-a"
   user_data    = data.template_file.user_data.rendered
